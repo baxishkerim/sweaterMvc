4f4f9fa5556ffabb6375675f81a4c1dccec95448
baxishkerim <developerkerimzade@gmail.com>
2022-03-08 23:19:51 +0400

    08.03

---
+++ Sweatermvc/src/test/java/com/example/sweatermvc/SweatermvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweatermvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweatermvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
