1aeb7ae864a57cd0943b7ec0b80787955d4dc3fe
baxishkerim <developerkerimzade@gmail.com>
2022-03-08 23:37:12 +0400

    tewer

--- Sweatermvc/src/main/java/com/example/sweatermvc/repos/MessageRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.sweatermvc.repos;
-
-import com.example.sweatermvc.domain.Message;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepo extends CrudRepository<Message,Long> {
-}
